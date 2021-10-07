d829262b88bfe89b43e7cf6664fe0273b53b9a47
Katlyn Boches <katlynboches@gmail.com>
2021-10-06 19:39:25 -0600

    Updated Read Me

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,17 @@ var showResults = document.getElementById("#results");
 var searchButton = document.getElementById("#search");
 
 
-var APIkey1 = ""
+var APIkey1 = "&appid=35fec87f63msh0eb478548ed1ee9p19d869jsnd2a13d4daaf9"
 
 function getApi() {
 
   // testing example fetch request
-  fetch('')
+  fetch('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com')
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('');
+    console.log('Moive Shown in:');
     console.log(data);
   });
 
