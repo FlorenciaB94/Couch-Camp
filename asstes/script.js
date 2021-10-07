461a31060a36f117fc0636c2c5ac07c0770defc7
Flor C <florenciabelenc94@gmail.com>
2021-10-06 18:25:44 -0600

    fixed html load error, fixed css and js links

--- asstes/script.js
+++ asstes/script.js
@@ -1,15 +1,29 @@
-var APIkey1 = "35fec87f63msh0ed1ee9p19d869jsnd2a13d4daaf9"
+console.log(this);
+var showResults = document.getElementById("#results");
+var searchButton = document.getElementById("#search");
 
-fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": APIkey1,
-		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+
+var APIkey1 = ""
+
+function getApi() {
+
+  // testing example fetch request
+  var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
+  
+  fetch(requestUrl)
+  .then(res => console.log(res));
+  
+  
+  
+  
+  
+  // (function (response) {
+    // return response.json();
+  // })
+  // .then(function (data) {
+  //   console.log(data)
+  // })
+};
+
+
+// searchButton.addEventListener('click', getApi);
