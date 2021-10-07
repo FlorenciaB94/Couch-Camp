e60a2500c71d6d1f9255b8fec854c1cbdf34542b
Flor C <florenciabelenc94@gmail.com>
2021-10-06 18:47:47 -0600

    fixed general issues

--- assets/script.js
+++ assets/script.js
@@ -8,12 +8,12 @@ var APIkey1 = ""
 function getApi() {
 
   // testing example fetch request
-  fetch('https://api.github.com/orgs/twitter/public_members')
+  fetch('')
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('Twitter Public Members: Raw data \n----------');
+    console.log('');
     console.log(data);
   });
 
