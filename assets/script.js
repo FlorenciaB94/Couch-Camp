b40854565871bf5eecbc42ccb3a6fdf09b5da3fa
Flor C <florenciabelenc94@gmail.com>
2021-10-06 21:49:17 -0600

    main JS logic

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,31 @@
 console.log(this);
-var showResults = document.getElementById("#results");
-var searchButton = document.getElementById("#search");
+var showResults = document.getElementById("results");
+var searchButton = document.getElementById("search");
+var input = document.getElementById("name");
+
+var APIkey1 = "b2603b30013667b374cd4d50875144c1"
 
 
-var APIkey1 = ""
-
-function getApi() {
-
+function getApi(event) {
+event.preventDefault();
+var movieTitle = input.value 
+console.log(movieTitle);
   // testing example fetch request
-  fetch('')
+  console.log(``)
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APIkey1}&query=${movieTitle}`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('');
+    console.log(data);
+    getMoviePlatforms(data.results[0].id);
+  });
+ 
+};
+// get movie ID's for searching 
+function getMoviePlatforms(movieID){
+  fetch(`https://api.themoviedb.org/3/movie/${movieID}/watch/providers?api_key=b2603b30013667b374cd4d50875144c1`)
   .then(function (response) {
     return response.json();
   })
@@ -16,18 +33,5 @@ function getApi() {
     console.log('');
     console.log(data);
   });
-
-  
-  
-  
-  
-  // (function (response) {
-    // return response.json();
-  // })
-  // .then(function (data) {
-  //   console.log(data)
-  // })
 };
-getApi();
-
-// searchButton.addEventListener('click', getApi);
+searchButton.addEventListener('click', getApi);
