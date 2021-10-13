d5750c72dc07b99a7ed74ed3e671e0b003bcdf8d
Flor C <florenciabelenc94@gmail.com>
2021-10-13 16:15:44 -0600

    Poster API fetch call

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,13 @@ var searchButton = document.getElementById("search");
 var input = document.getElementById("name");
 var cardRow = document.getElementById('card_row');
 
-//key for TheMovieDatabase API
+//Key for TheMovieDatabase API
 var APIkey1 = "b2603b30013667b374cd4d50875144c1"
-//key for NYT reviews
+//Key for NYT reviews API
 var APIkey2 = "1eWGISZRchA7XQkb0Mw5vUd1Age2qfKE"
 
 // Movies API fetch request
 async function getMovies(movieTitle) {
-  
   return await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APIkey1}&query=${movieTitle}`)
   .then(function (response) {
     return response.json();
@@ -30,18 +29,37 @@ async function getMovies(movieTitle) {
   });
 };
 
-// get movie ID's for searching 
+// Get movie ID's for searching 
 async function getMoviePlatforms(movieID){
   return await fetch(`https://api.themoviedb.org/3/movie/${movieID}/watch/providers?api_key=${APIkey1}`)
   .then(function (response) {
     return response.json();
   })
 };
+
+// Get movie posters for results
+async function getPosters(movieTitle) {
+  return await fetch(`https://api.themoviedb.org/3/movie/${movieID}/images?api_key=${APIkey1}&language=en-US`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) { 
+    var postersData = [];
+    var posters = data.results;
+    for (var p = 0; p< 6; p++ ){
+      var poster = posters[p];
+        postersData.push(
+        {
+         poster: poster.file_path
+        })
+    }
+   return postersData;
+  });
+};
+
 // Reviews API fetch call
-
 async function getReviews(movieID) {
-
-  return await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?&query=${movieID}&api-key=${APIkey2}`)
+ return await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?&query=${movieID}&api-key=${APIkey2}`)
   .then(function (response) {
     return response.json();
   })
@@ -61,19 +79,21 @@ async function getReviews(movieID) {
 
 
 
-
-
+// Pulling all data together
 async function fetchMovieData(event) {
   event.preventDefault();
   cardRow.innerHTML = "";
-// call getMovies function first
+
+  // Call getMovies function first
   var movieData = await getMovies(input.value);
 
-  // then call getMoviePlatforms next
+// Then call getMoviePlatforms next
   for (var i = 0; i <  6; i++) {
     var streamingData = await getMoviePlatforms(movieData[i].movieId);
     if (streamingData.results.length === 0) {
-      // do something
+      // Create a card with a descriptive message
+      cardRow.innerHTML += `<div class="movie_card column"><div class="callout"><p>Your search did not produce any results</p></div></div>`
+      // Streaming Data
     } else {
       var streamingDataInUS = streamingData.results["US"];
       var rent = streamingDataInUS?.rent || [];
@@ -83,39 +103,29 @@ async function fetchMovieData(event) {
 
       }
       movieData[i].providers = providers;
-
-    }
- 
-  }
+    }}
 
   console.log("---- movie data after getting streaming data");
   console.log(movieData)
-
+// Review Data
  for (var r= 0; r < 6; r++){
   console.log("------------review Data stuff"); 
   console.log(movieData[r]);
      var reviewsContent = await getReviews(movieData[r].movieTitle);
          console.log(movieData);
         movieData[r].review = reviewsContent;
-        
-        // now we need to dynamically plug in  all the data we have in movieData to the cards on html
-
-  
-      
-
-
+      }
+// Poster Data
+ for (var p=0; p < 6; p++){
+   console.log("---------poster data")
+   console.log(movieData[p]);
+   var postersContent = await getPosters(movieData[p].movieTitle);
+   console.log(movieData);
+   movieData[p].poster = postersContent;
  }
-searchButton.addEventListener('click', fetchMovieData);
- 
-}
 
-        cardRow.innerHTML += `<div class="movie_card column"><div class="callout"><h4>${movieData[r].movieTitle}</h4><p class="lead"></p><a href="${movieData[r].review[0]}" target="_blank"><p>review</p></a></div></div>`;
-      } 
-
-}
-      
-
-
-
+  // now we need to dynamically plug in  all the data we have in movieData to the cards on html
+  cardRow.innerHTML += `<div class="movie_card column"><div class="callout"><h4>${movieData[r].movieTitle}</h4><p class="lead"></p><a href="${movieData[r].review[0]}" target="_blank"><p>review</p></a></div></div>`;
+ }
 searchButton.addEventListener('click', fetchMovieData);
 
